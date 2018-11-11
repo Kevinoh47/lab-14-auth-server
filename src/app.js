831c67cf45ae3b640e22946177a48bfdf49a72dc
Kevinoh47 <kohok47@gmail.com>
2018-11-10 16:23:40 -0800

    not yet

--- src/app.js
+++ src/app.js
@@ -5,19 +5,67 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-
 import authRouter from './auth/router.js';
 import auth from './auth/middleware.js';
 import errorHandler from './middleware/error.js';
 import notFound from './middleware/404.js';
 
+import session from 'express-session';
+import passport from 'passport';
+import Auth0Strategy from 'passport-auth0';
+
 let app = express();
+
+let sess = {
+  secret: process.env.AUTH0_COOKIE_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+};
+
+if (app.get('env') === 'production') {
+  sess.cookie.secure = true; // serve secure cookies, requires https
+}
+
+app.use(session(sess));
+
+var strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback',
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// You can use this section to keep a smaller payload
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());  // => req.body
 app.use(express.urlencoded({extended:true})); // req.body => from a form's key value pairs
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(authRouter);
 
